726ec15e7c8b05b6260bced0ebe43fcd287f37dd
AaronJan <aaronjan@qq.com>
2014-12-11 16:30:06 +0800

    make sed replace global

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -4,6 +4,6 @@ set -e
 rm -Rf /etc/nginx
 cp -R /etc/nginx_readonly /etc/nginx
 
-sed -i "s/<PHPFPM_HOST>/${PHPFPM_PORT_9000_TCP_ADDR}/;s/<PHPFPM_PORT>/${PHPFPM_PORT_9000_TCP_PORT}/" /etc/nginx/conf.d/vhost.conf;
+sed -i "s/<PHPFPM_HOST>/${PHPFPM_PORT_9000_TCP_ADDR}/g;s/<PHPFPM_PORT>/${PHPFPM_PORT_9000_TCP_PORT}/g" /etc/nginx/conf.d/vhost.conf;
 
 exec "$@"
